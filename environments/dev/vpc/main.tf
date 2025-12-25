ce45aae13b5cbb89832f585f2db09a56c6ebfb5c
rajeshkothinti <rajesh.kothinti@alpheya.com>
2025-12-25 16:16:27 +0530

    ci file and vpc

--- environments/dev/vpc/main.tf
+++ environments/dev/vpc/main.tf
@@ -1,7 +1,7 @@
 module "vpc" {
-  source = "../../modules/vpc"
+  source = "../../../modules/vpc"
 
-  name                  = var.vpc_name
+  vpc_name                  = var.vpc_name
   vpc_cidr              = var.vpc_cidr
   enable_dns_support    = var.enable_dns_support
   enable_dns_hostnames  = var.enable_dns_hostnames
